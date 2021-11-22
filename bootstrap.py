fdd810d028aaae873dad3616fffadbe5f456cee7
albanberisha <alban.berisha@studentet.uni-pr.edu>
2021-11-22 01:18:26 +0100

    Organizimi i follderave

---
+++ bootstrap.py
@@ -0,0 +1,30 @@
+import sys
+from os import path
+import pathlib
+from os import walk
+
+# Count the arguments
+arguments = len(sys.argv) - 1
+error=0
+if arguments>2 or arguments<2:
+    print("Required arguments 2.")
+    print("Options.\n --first-phase [date]")
+else:
+    # Output argument-wise
+    first_arg=sys.argv[1]
+    if first_arg=='--first-phase':
+        date = sys.argv[2]
+        print(f"-- Checking if directory {date} exists in path {pathlib.Path(__file__).parent.resolve()}\sources")
+        if(path.isdir(f'sources/{date}')):
+            print(f"-- Directory {date} found in path \n{4*' '}{pathlib.Path(__file__).parent.resolve()}\sources \n")
+            print(f"-- Listing files inside {pathlib.Path(__file__).parent.resolve()}\sources\{date} directory")
+            path=f"{pathlib.Path(__file__).parent.resolve()}\sources\{date}"
+            f = []
+            for (dirpath, dirnames, filenames) in walk(path):
+                if(dirpath==path):
+                    f.extend(filenames)
+            print(f)
+        else:
+            print(f"ERROR: Given directory: \n {4*' '}{pathlib.Path(__file__).parent.resolve()}/sources/{date} \n {2*' '}could not be found. \nProcess terminated with status error {error}. ")
+    else:
+        print("Options.\n --first-phase [date]")
