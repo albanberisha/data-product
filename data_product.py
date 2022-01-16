e91a42fdf3dd892e445ee8fc826c719640af73a3
albanberisha <alban.berisha@studentet.uni-pr.edu>
2022-01-16 22:48:38 +0100

    Added visualization phase

--- data_product.py
+++ data_product.py
@@ -34,7 +34,7 @@ def validateFile(file,path,currentDir):
     savedataSet(dataset,currentDir,summary)
 
 def secondPhase(currentDir):
-    firstphasesource=["Artworks_clean.csv"]
+    secondphasesource=["Artworks_clean.csv"]
     print(f"-- Listing files inside {pathlib.Path(__file__).parent.resolve()}\\first_phase_output\{currentDir} directory")
     path = f"{pathlib.Path(__file__).parent.resolve()}\\first_phase_output\\{currentDir}"
     f = []
@@ -50,25 +50,21 @@ def secondPhase(currentDir):
             validation_files = []
             # check if excepted file is inside source
             for file in f:
-                if file in firstphasesource and validation:
+                if file in secondphasesource and validation:
                     count += 1
                     validation_files.append(file)
 
             # if files inside arent the one we expected
             result=False
             if count == 0:
-                print(f"ERROR: Files  inside: \n {4 * ' '}{pathlib.Path(__file__).parent.resolve()}//first_phase_output//{currentDir} \n {2 * ' '} does correspond with excepted file names.\n {2 * ' '} Missing: {firstphasesource} file")
+                print(f"ERROR: Files  inside: \n {4 * ' '}{pathlib.Path(__file__).parent.resolve()}//first_phase_output//{currentDir} \n {2 * ' '} does correspond with excepted file names.\n {2 * ' '} Missing: {secondphasesource} file")
             elif count == 1:
-                print(f"-- Files {firstphasesource} inside {pathlib.Path(__file__).parent.resolve()}\ first_phase_output\{currentDir} found")
-                validateFile(file,path,currentDir)
+                print(f"-- Files {secondphasesource} inside {pathlib.Path(__file__).parent.resolve()}\ first_phase_output\{currentDir} found")
+                validateFile(secondphasesource[0],path,currentDir)
 
 
     else:
         print(f"-- Directory: \n {4 * ' '}{pathlib.Path(__file__).parent.resolve()}/first_phase_output/{currentDir} \n is empty. \nProcess terminated with status error {error}. ")
-
-
-
-
 
 # Count the arguments
 arguments = len(sys.argv) - 1
@@ -90,7 +86,7 @@ else:
     if first_arg == '--second-phase':
         date = sys.argv[2]
         print(f"-- Checking if directory {date} exists in path {pathlib.Path(__file__).parent.resolve()}\\first_phase_output")
-        if (path.isdir(f'sources/{date}')):
+        if (path.isdir(f'first_phase_output/{date}')):
             print(f"-- Directory {date} found in path \n{4 * ' '}{pathlib.Path(__file__).parent.resolve()}\\first_phase_output \n")
             secondPhase(date)
         else:
